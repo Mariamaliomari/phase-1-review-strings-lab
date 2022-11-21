e130ae3e89b816b7e34424f2c9a199dfe6330e7e
Omari99 <mariam.ali01@student.moringaschool.com>
2022-11-21 16:25:32 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+
+const currentUser = 'Mariam Ali';
+const welcomeMessage = 'Welcome to Flatbook, Mariam Ali!';
+
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, MARIAM ALI!";
+const shortGreeting = "Welcome, M!";
